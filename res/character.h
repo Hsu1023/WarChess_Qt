ad4089a407daa5d16bb03974f14dc3fca2efd24c
Hsv1023 <xuhb2001@gmail.com>
2021-09-04 19:55:45 +0800

    Revert "release v1.3 以qt的封装模式重写原有功能"
    This reverts commit 3a3c2f9760b26084531ef22b2cc453320417f3e2.

--- res/character.h
+++ res/character.h
@@ -2,25 +2,22 @@
 #define CHARACTER_H
 
 #include "config.h"
-#include "characterlabel.h"
 #include <QWidget>
 
-class Character: public QLabel
+class Character: public QWidget
 {
     Q_OBJECT
 public:
     Character(int t_cell_x, int t_cell_y, int LocalScreenx, int LocalScreeny, QWidget* parent = nullptr);
 public:
-    int  m_hp, m_fullhp, m_move,m_fullmove,m_attrack;
+    int m_attrack, m_move, m_fullblood, m_blood;
     int m_cellx, m_celly; //  全局位置
     int m_localCellx, m_localCelly; // 桌面位置
     QPixmap icon;
-    CharacterLabel *dlg;
+    QLabel *dlg;
     QString name;
 public:
     void setLabel();
-    void enterEvent(QEvent *)override;
-    void leaveEvent(QEvent *)override;
 };
 
 class Warrior: public Character
--- res/chess.pro
+++ res/chess.pro
@@ -10,14 +10,12 @@ CONFIG += c++11
 
 SOURCES += \
     character.cpp \
-    characterlabel.cpp \
     main.cpp \
     maindialog.cpp \
     map.cpp
 
 HEADERS += \
     character.h \
-    characterlabel.h \
     config.h \
     maindialog.h \
     map.h
@@ -29,5 +27,3 @@ else: unix:!android: target.path = /opt/$${TARGET}/bin
 
 RESOURCES += \
     pic.qrc
-
-DISTFILES +=
